908baad91a6358b61ec8d4e59ac86770480917a1
진영 <81845425+baejin1018@users.noreply.github.com>
2022-09-01 20:59:58 +0900

    solved : 같은숫자는 싫어

---
+++ 프로그래머스/1단계/같은숫자는싫어.js
@@ -0,0 +1,19 @@
+const arr = [1, 1, 3, 3, 0, 1, 1];
+/*
+function solution(arr) {
+  var answer = [];
+
+  arr.forEach((d, i) => {
+    if (d != arr[i + 1]) answer.push(arr[i]);
+  });
+  return answer;
+}
+console.log(solution(arr));
+*/
+//===========================
+
+function solution(arr) {
+  var answer = [];
+  return arr.filter((d, i) => d != arr[i + 1]);
+}
+console.log(solution(arr));
